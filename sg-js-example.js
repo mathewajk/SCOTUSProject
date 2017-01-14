f2949e2473cabb8772c4dd3805a47d369d8f6066
Allison Kramer <allisonkramer@uchicago.edu>
2017-01-14 12:16:39 -0600

    minor clarity edit

--- sg-js-example.js
+++ sg-js-example.js
@@ -1,7 +1,4 @@
 $SG(document).ready(function(){
-  	$SG( '.insert-audio-1' ).html('<source src="http://projects.rcc.uchicago.edu/aclyu/vot/' + '[url("a1")]' + '.wav"></source>');
-  	$SG( '.insert-audio-2' ).html('<source src="http://projects.rcc.uchicago.edu/aclyu/vot/' +  '[url("a2")]' + '.wav"></source>');
-  	var audio3 = $SG( '.insert-audio-3' );
-  	console.log(audio3);
-  	audio3.html('<source src="http://projects.rcc.uchicago.edu/aclyu/vot/' + '[url("a3")]' + '.wav"></source>');
+  	var audio1 = $SG( '.insert-audio-3' );
+  	audio1.html('<source src="http://projects.rcc.uchicago.edu/aclyu/vot/' + '[url("a3")]' + '.wav"></source>');
  });
