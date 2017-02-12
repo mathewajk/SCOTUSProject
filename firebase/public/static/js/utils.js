a63007dc62603a9000dd29124ae3c54912c41ca8
Syuurinn <allisonkramer@uchicago.edu>
2017-02-12 15:24:37 -0600

    Added function to convert object array to CSV-formatted string

--- firebase/public/static/js/utils.js
+++ firebase/public/static/js/utils.js
@@ -9,6 +9,38 @@ function saveData(filedata, dataRef){
     });
 }
 
+function objArrayToCSV(args) {  
+    var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+    data = args.data || null;
+    if (data == null || !data.length) {
+        return null;
+    }
+
+    columnDelimiter = args.columnDelimiter || ',';
+    lineDelimiter = args.lineDelimiter || '\n';
+
+    keys = Object.keys(data[0]);
+
+    result = '';
+    result += keys.join(columnDelimiter);
+    result += lineDelimiter;
+
+    data.forEach(function(item) {
+        ctr = 0;
+        keys.forEach(function(key) {
+            if (ctr > 0) result += columnDelimiter;
+
+            result += item[key];
+            ctr++;
+        });
+        result += lineDelimiter;
+    });
+
+    return result;
+}
+
+
 // Check whether or not a worker has completed the experiment
 function checkWorker(workerId) {
     return firebase.database().ref('workers/' + workerId).once('value').then(function(snapshot) {
