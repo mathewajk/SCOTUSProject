0629ddc3f5b5fb45509505a10422c00562afe64a
Narquelion <a.kramer103@gmail.com>
2018-01-11 17:13:49 -0600

    Add stock audio

--- firebase/public/sample/resources/data/audio.js
+++ firebase/public/sample/resources/data/audio.js
@@ -1,3 +1,21 @@
 // audio.js
 // Define audio filenames
-var audioNames = [];
+
+var audioNames =[
+  "001_big_orig.wav",
+  "001_trip_orig.wav",
+  "002_big_orig.wav",
+  "002_trip_orig.wav",
+  "003_big_orig.wav",
+  "003_trip_orig.wav",
+  "004_big_orig.wav",
+  "004_trip_orig.wav",
+  "005_big_orig.wav",
+  "005_trip_orig.wav",
+  "006_big_orig.wav",
+  "006_trip_orig.wav",
+  "007_big_orig.wav",
+  "008_trip_orig.wav",
+  "008_big_orig.wav",
+  "008_trip_orig.wav"
+];
